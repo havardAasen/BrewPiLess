2100dd15700272a598a8eb1d3a313a78e2172d7a
Håvard F. Aasen <havard.f.aasen@pfft.no>
2023-02-23 22:33:33 +0100

    html: Minor fixes in script to generate compiled html files
    - Change shebang to 'bash', we use arrays extensively and these are not
      supported in regular Bourne shell.
    - Remove unused variable.
    - Add '-f' to 'rm' command, this generated a warning when the directory
      don't exists.
    - Quoted remaining variables, mostly to keep shellcheck happy.

--- htmljs/output.sh
+++ htmljs/output.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 OUTDIR="cheader"
-DISTDIR="dist"
 
 if [ ! -d $OUTDIR ]; then
     echo "$OUTDIR not found!"
     mkdir "$OUTDIR"
 fi
-rm $OUTDIR/*.h
+rm -f $OUTDIR/*.h
 
 htmlfiles=(index_s.htm.gz control_s.htm.gz config.htm.gz setup.htm.gz logging.htm.gz gravity.htm.gz gravity_e32.htm.gz pressure.htm.gz backup.htm.gz)
 
@@ -29,13 +28,12 @@ for ((index=0; index<${#htmlfiles[@]}; index++)); do
    output="$OUTDIR/${lang}_${outfiles[$index]}.h"
    variable=${variables[$index]}
    #echo "input: $input output file: $output with variables $variable "
-   xxd -i  "$input" > $output 
+   xxd -i "$input" > "$output"
    echo "processing $output"
-   sed -i "s/unsigned char .\+\[\]/const unsigned char $variable\[\] PROGMEM/" $output
+   sed -i "s/unsigned char .\+\[\]/const unsigned char $variable\[\] PROGMEM/" "$output"
 done
 }
 
-for lang in "${languages[@]}"
-do
-gen_C_file $lang
+for lang in "${languages[@]}"; do
+   gen_C_file "$lang"
 done

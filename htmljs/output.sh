d4632969957bc3eb74c94737da2e58d9bd1157cf
Håvard F. Aasen <havard.f.aasen@pfft.no>
2024-06-29 21:13:20 +0200

    Frontend: Create header files from all required files

--- htmljs/output.sh
+++ htmljs/output.sh
@@ -9,13 +9,13 @@ fi
 rm -f $OUTDIR/*.h
 
 htmlfiles=(index_s.htm.gz control_s.htm.gz config.htm.gz setup.htm.gz logging.htm.gz gravity.htm.gz pressure.htm.gz)
-
 variables=(data_index_htm_gz control_htm_gz config_htm_gz setup_htm_gz logging_htm_gz gravity_htm_gz pressure_htm_gz)
-
 outfiles=(index_htm control_htm config_htm setup_htm log_htm gdc_htm pressure_htm)
-
 languages=(english spanish portuguese-br slovak chinese)
 
+non_translated_files=(lcd.htm.gz edit.htm.gz testcmd.htm.gz bwf.gz.js dygraph.gz.js)
+on_translated_variables=(lcd_htm_gz edit_htm_gz data_testcmd_htm data_bwf_min_js_gz dygraph_combined_js_gz)
+non_translated_out=(lcd_htm edit_html_gz testcmd_htm bwf_js dygraph_js)
 
 gen_C_file()
 {
@@ -36,4 +36,13 @@ done
 
 for lang in "${languages[@]}"; do
    gen_C_file "$lang"
+done
+
+for ((index=0; index<${#non_translated_files[@]}; index++)); do
+   input="dist/${non_translated_files[$index]}"
+   output="$OUTDIR/${non_translated_out[$index]}.h"
+   variable=${on_translated_variables[$index]}
+   xxd -i "$input" > "$output"
+   echo "processing $output"
+   sed -i "s/unsigned char .\+\[\]/const unsigned char $variable\[\] PROGMEM/" "$output"
 done

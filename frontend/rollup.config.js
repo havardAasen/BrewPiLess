fec8bae3427c7ee0939cb6cf65be13f7103a41a4
Håvard F. Aasen <havard.f.aasen@pfft.no>
2025-09-30 21:52:04 +0200

    [frontend] Start using TypeScript
    Only one file has been converted to TS, no changes was needed other then
    renaming the file to .ts.

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,17 +1,20 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
-  input: 'src/js/main.js',
+  input: 'src/js/main.ts',
   output: {
     file: 'dist/bundle.js',
     format: 'es',
     sourcemap: false
   },
   plugins: [
+    typescript(),
     resolve(),
     commonjs(),
     ...(isProduction ? [terser()] : [])

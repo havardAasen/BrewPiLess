2ab525da33c3d8ca6002b9adb655ea6fd2649a90
Håvard F. Aasen <havard.f.aasen@pfft.no>
2025-10-26 07:58:25 +0100

    [ts] Rewrite gravity from JavaScipt to TypeScript

---
+++ frontend/src/js/script-gravity.ts
@@ -0,0 +1,64 @@
+import { select, getActiveNavItem, JSVERSION, s_ajax } from './shared';
+
+const gdcurl = "/gdc";
+
+function fill(setting: Record<string, any>): void {
+    for (const name in setting) {
+        const element = select(`input[name=${name}]`) as HTMLInputElement | null;
+        if (element) {
+            if (element.type === "checkbox") {
+                element.checked = Boolean(setting[name]);
+            } else {
+                element.value = String(setting[name]);
+            }
+        }
+    }
+}
+
+function save(): void {
+    const inputs = document.getElementsByTagName("input");
+    const setting: Record<string, any> = {};
+    Array.from(inputs).forEach((ele) => {
+        if (!ele.name) return;
+        if (ele.type === "checkbox") {
+            setting[ele.name] = ele.checked;
+        } else if (ele.type === "text") {
+            setting[ele.name] = ele.value;
+        }
+    });
+    //    console.log("result=" + setting);
+
+    s_ajax({
+        url: gdcurl,
+        m: "POST",
+        mime: "application/json",
+        data: JSON.stringify(setting),
+        success: function(a: any) {
+            alert("<%= done %>");
+        },
+        fail: function(a: any) {
+            alert("<%= script_control_failed_updating_data %>" + a)
+        }
+    });
+}
+
+export function init(): void {
+    getActiveNavItem();
+    const verinfo = select("#verinfo") as HTMLElement | null;
+    if (verinfo) {
+        verinfo.innerHTML = `v${JSVERSION}`;
+    }
+
+    s_ajax({
+        url: gdcurl + "?data",
+        m: "GET",
+        success: function(a: string) {
+            fill(JSON.parse(a));
+        },
+        fail: function(a: any) {
+            //alert("failed getting data:" + a)
+        }
+    });
+}
+
+(window as any).save = save;

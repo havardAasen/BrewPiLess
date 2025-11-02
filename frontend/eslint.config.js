0c3f3fbc80c554479c93930cf60eb85a412e59a8
Håvard F. Aasen <havard.f.aasen@pfft.no>
2025-11-02 18:33:04 +0100

    Update ESLint to lint TypeScipt

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,35 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js }, extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: globals.browser,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
 ]);

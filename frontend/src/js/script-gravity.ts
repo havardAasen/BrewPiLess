569dc34cae56a28bcbc8bf5d9c2cd3eeb6a552d7
Håvard F. Aasen <havard.f.aasen@pfft.no>
2026-06-20 07:47:55 +0200

    [frontend] Wrap HTTP input fields in a form, gravity page

--- frontend/src/js/script-gravity.ts
+++ frontend/src/js/script-gravity.ts
@@ -1,4 +1,4 @@
-import { select, updateNavbarVersion } from "./shared";
+import { byId, select, updateNavbarVersion } from "./shared";
 import { get, post } from "./httpClient";
 
 const gdcurl = "/gdc";
@@ -41,6 +41,11 @@ async function save(): Promise<void> {
 export async function init(): Promise<void> {
     updateNavbarVersion();
 
+    byId<HTMLFormElement>("gravity-form")!.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+        save();
+    });
+
     try {
         const json = await get(`${gdcurl}?data`);
         fill(JSON.parse(json));
@@ -48,5 +53,3 @@ export async function init(): Promise<void> {
         console.warn(error);
     }
 }
-
-window.Save = save;
